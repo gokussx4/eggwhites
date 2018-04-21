eba641c7a51e228d13d75c74c38c268375ee62b8
goku <noway@gmail.com>
2018-04-21 12:38:27 -0400

    Add device component

--- fastresponse-ui/src/app/app-routing.module.ts
+++ fastresponse-ui/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule,Routes} from '@angular/router';
 import {RegisterComponent} from './register/register.component';
+import {DeviceComponent} from './device/device.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/register', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent }
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'device', component: DeviceComponent}
 ];
 
 @NgModule({
--- fastresponse-ui/src/app/app.module.ts
+++ fastresponse-ui/src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DeviceComponent } from './device/device.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    DeviceComponent
   ],
   imports: [
     BrowserModule,
     MatFormFieldModule,
     MatInputModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
