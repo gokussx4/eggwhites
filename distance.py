14598b48ebb237c9db9315777f947d6c6005c1d7
Andrew Hoblitzell <ahoblitz@users.noreply.github.com>
2018-04-21 15:40:14 -0400

    add dictionary key preservation

--- distance.py
+++ distance.py
@@ -1,29 +1,6 @@
 import numpy as np
 import operator
-
-def find_key(dic, val):
-    """return the key of dictionary dic given the value"""
-    return [k for k, v in val.values if v == val][0]
-
-def find_value(dic, key):
-    """return the value of dictionary dic given the key"""
-    return dic[key]
-
-class Lookup(dict):
-    """
-    a dictionary which can lookup value by key, or keys by value
-    """
-    def __init__(self, items=[]):
-        """items can be a list of pair_lists or a dictionary"""
-        dict.__init__(self, items)
-
-    def get_key(self, value):
-        """find the key(s) as a list given a value"""
-        return [item[0] for item in self.items() if item[1] == value]
-
-    def get_value(self, key):
-        """find the value given a key"""
-        return self[key]
+import collections
 
 def spherical_dist(pos1, pos2, r=3958.75):
     pos1 = pos1 * np.pi / 180
@@ -40,11 +17,21 @@ def get_sorted_dist(distances):
     return sortedDistanceMap
 
 def main():
-    pos1=np.array([39.956755, -86.01335])
-    pos2=np.array([[39.956755, -86.01335],[38.956755, -86.01335],[39.956755, -86.01335],[39.956755, -86.01335]])
-    print(get_sorted_dist(spherical_dist(pos1, pos2)))
+    #initialize data
+    pos1=np.array([0, 0])
+    pos2Dictionary = collections.OrderedDict()
+    pos2Dictionary.update({'user1': [4, 4]})
+    pos2Dictionary.update({'user2': [3, 3]})
+    pos2Dictionary.update({'user3': [2, 2]})
+    pos2Dictionary.update({'user4': [0, 0]})
+    pos2Dictionary.update({'user5': [1, 1]})
 
+    pos2 = np.array(pos2Dictionary.values())
+    result = collections.OrderedDict()
+    sorted = get_sorted_dist(spherical_dist(pos1, pos2))
+    for k,v in sorted:
+        result[pos2Dictionary.keys()[k-1]] = pos2Dictionary.values()[k-1]
+    print(result)
 
-if __name__=="__main__":
-
+if __name__== "__main__":
     main()
